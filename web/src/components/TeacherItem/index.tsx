c13a807794c74d44e838e6a1e18177694efce197
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-08-07 17:45:33 -0300

    Day One

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars2.githubusercontent.com/u/5805116?s=460&u=9f2befdf70aa0e2b69f1733df13251036d37ad8e&v=4"
+          alt="Carlos Oliveira"
+        />
+        <div>
+          <strong>Carlos Oliveira</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
